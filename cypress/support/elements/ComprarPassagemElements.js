b53c5511faec6216b45ac7c6e15ef50360f41f5d
tcthiagoramalho <thiago.barbosa@tc.com.br>
2021-08-07 20:14:15 -0300

    FluxoCompleto

--- cypress/support/elements/ComprarPassagemElements.js
+++ cypress/support/elements/ComprarPassagemElements.js
@@ -1,8 +1,23 @@
 class comprarPassagemElements {
-    Origem = () => {return '.with-margin > .vertical-align-flights > .flight-legs > .container-origin > .label-from-to > .lbl'}
+    Origem = () => {return '#inputOrigin'}
     SetOrigem = () => {return '#ulOriginAirport > :nth-child(3)'}
     Destino = () => {return '#inputDestination'}
     SetDestino = () => {return '#ulDestinationAirport > :nth-child(1)'}
+
+    Calendario = () => {return '#_smilesflightsearchportlet_WAR_smilesbookingportlet_departure_date'}
+    DataIda = () => {return '.ui-datepicker-group-first > .ui-datepicker-calendar > tbody > :nth-child(3) > :nth-child(1) > .ui-state-default'}
+    DataVolta = () => {return '.ui-datepicker-group-first > .ui-datepicker-calendar > tbody > :nth-child(5) > :nth-child(3) > .ui-state-default'}
+    ConfirmeData = () => {return '.ui-datepicker-current'}
+
+    BuscarVoo = () => {return '#submitFlightSearch'}
+
+    VooIda = () => {return '#firstFlights > .segmentsFlightsTemplate > .smiles__flight-search > #flightsArticle0 > :nth-child(4) > .column-miles > .miles > [style="position:relative"] > .checkbox > .flightlb'}
+    VooVolta = () => {return '#secondFlights > .segmentsFlightsTemplate > .smiles__flight-search > #flightsArticle0 > :nth-child(4) > .column-miles > .miles > [style="position:relative"] > .checkbox > .flightlb'}
+
+    SetTermo = () => {return '.terms > label'}
+    Continuar = () => {return '.button > .btn'}
+
+    Login = () => {return '.main-content > h3'}
 }
 
 export default comprarPassagemElements
--- cypress/support/pageobjects/ComprarPassagemPege.js
+++ cypress/support/pageobjects/ComprarPassagemPege.js
@@ -8,27 +8,35 @@ const passagem = new comprarPassagemElements
 class comprarPassagem {
 
     PrencherEndereco() {
-        cy.get(passagem.Origem()).click().type('São Paulo')
+        cy.get(passagem.Origem()).type('São Paulo')
         cy.get(passagem.SetOrigem()).click()
         cy.get(passagem.Destino()).type('Rio de janeiro')
         cy.get(passagem.SetDestino()).click()
     }
 
     PrencherData() {
-
+        cy.get(passagem.Calendario()).click()
+        cy.get(passagem.DataIda()).click({force: true})
+        cy.get(passagem.DataVolta()).click({farce: true})
+        cy.get(passagem.ConfirmeData()).click({force: true})
+        cy.get(passagem.BuscarVoo()).click()
     }
 
     SelecionarVoo() {
-
-    }
-
-    Termos() {
-
+        cy.get(passagem.VooIda()).click()
+        cy.get(passagem.VooVolta()).click()
     }
 
     VizualizarResumo() {
 
     }
+
+    AceitarTermos() {
+        cy.get(passagem.SetTermo()).click()
+        cy.get(passagem.Continuar()).click()
+
+        cy.get(passagem.Login()).should('contain', 'Acesse sua conta')
+    } 
 }
 
 export default comprarPassagem;
